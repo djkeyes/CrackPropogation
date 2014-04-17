2d2c63ae0601b9beba71b381edaef3bcdbba5219
Daniel Keyes <dkeyes6@gatech.edu>
2014-04-17 19:13:33 -0400

    some class layouts

--- java/src/crackSim/scheduling/TimeWarpScheduler.java
+++ java/src/crackSim/scheduling/TimeWarpScheduler.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 // performs scheduling by running processes in parallel and using the TimeWarp algorithm to resolve problems when two processes conflict
+// implementation: this has a few inner classes to handle process creation
 public class TimeWarpScheduler implements Scheduler {
 
 	private List<IODevice> ioDevices;
@@ -15,8 +16,7 @@ public class TimeWarpScheduler implements Scheduler {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		// start by launching an initializer process
 	}
 
 	@Override
@@ -24,4 +24,37 @@ public class TimeWarpScheduler implements Scheduler {
 		ioDevices.add(ioDevice);
 	}
 
+	// runs a process for making new cracks. launches CrackPropagatorProcess threads.
+	private class CrackInitializerProcess implements Runnable {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	// runs a process for updating a CrackPropagator, sending messages, and performing rollbacks when necessary
+	private class CrackPropagatorProcess implements Runnable {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	// runs a process for Io events; it just calculated the minimum global time and displays the macro simulation at that level.
+	// at a smaller level, it shows the micro level simulatoins
+	private class IoProcess implements Runnable {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
 }

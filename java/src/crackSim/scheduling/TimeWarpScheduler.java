bc21771d645fe3f8b3ed56c0d43ae19296d16fd5
Daniel Keyes <dkeyes6@gatech.edu>
2014-04-17 19:55:04 -0400

    some tw stuff

--- java/src/crackSim/scheduling/TimeWarpScheduler.java
+++ java/src/crackSim/scheduling/TimeWarpScheduler.java
@@ -1,19 +1,38 @@
 package crackSim.scheduling;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
+import crackSim.core.BackingGrid;
+import crackSim.core.CAUpdateCalculator;
+import crackSim.core.CrackInitializer;
+import crackSim.core.CrackPropagator;
+import crackSim.core.Grid;
 
 // performs scheduling by running processes in parallel and using the TimeWarp algorithm to resolve problems when two processes conflict
 // implementation: this has a few inner classes to handle process creation
 public class TimeWarpScheduler implements Scheduler {
 
 	private List<IODevice> ioDevices;
+	private List<CrackPropagator> propagators;
 
-	public TimeWarpScheduler() {
-
+	public TimeWarpScheduler(BackingGrid macroBackingGrid, BackingGrid microBackingGrid, CAUpdateCalculator uc) {
 		ioDevices = new LinkedList<IODevice>();
-	}
 
+		// initializer = new CrackInitializer(uc);
+		// each crack has its own micro-scale grid. feed this grid the initializer.
+		CrackInitializer initializer = new CrackInitializer(uc, microBackingGrid);
+
+
+		final Grid g = new Grid(macroBackingGrid);
+
+		// initial update
+		for (IODevice ioDevice : ioDevices) {
+			ioDevice.update(g, 0, propagators);
+		}
+		
+	}
 	@Override
 	public void run() {
 		// start by launching an initializer process
@@ -29,8 +48,18 @@ public class TimeWarpScheduler implements Scheduler {
 
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
-
+			
+			// initialization
+			
+			
+			
+			// start up some processes
+			IoProcess iop = null;
+			CrackInitializerProcess cip = null;
+			
+			
+			new Thread(cip).start();
+			new Thread(iop).start();
 		}
 
 	}

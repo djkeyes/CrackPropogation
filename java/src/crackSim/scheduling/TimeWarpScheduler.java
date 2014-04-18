e509e60a69c268d3fd7eb008378b19c59b22b78b
Daniel Keyes <danieljkeyes@gmail.com>
2014-04-18 08:45:36 -0400

    slight tweak for prettier visualization. added a thread sleep to each propagator process so you can actually see what's going on.

--- java/src/crackSim/scheduling/TimeWarpScheduler.java
+++ java/src/crackSim/scheduling/TimeWarpScheduler.java
@@ -119,6 +119,12 @@ public class TimeWarpScheduler implements Scheduler {
 
 				// then update
 				crack.update();
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 
 		}
